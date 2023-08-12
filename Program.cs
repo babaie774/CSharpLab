fd587e4a30feaae04c085ff21258d125f7c26380
alireza babaie <60476541+babaie774@users.noreply.github.com>
2023-08-12 22:23:27 +0330

    implement-sum

--- Program.cs
+++ Program.cs
@@ -5,14 +5,22 @@ namespace controlApp1
 class Program
 {
     static void Main(string[] args) {
-        Console.ReadKey();
-        string Name = "Iman";
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        Console.WriteLine(c);
+        // Console.ReadKey();
+        // string Name = "Iman";
+        // int a = 1;
+        // int b = 2;
+        // int c = a + b;
+        // Console.WriteLine(c);
 
-        
+        int a;
+        int b;
+        int c = a + b;
+
+        Console.WriteLine("inter Number:");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("inter Number:");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(c);
     }
 } 
 class Iman 

61bc2175626e8772cf09d603b61ebd029f2320b1
alireza babaie <60476541+babaie774@users.noreply.github.com>
2023-08-12 22:09:32 +0330

    exp-strin-and-int

--- Program.cs
+++ Program.cs
@@ -1,52 +1,21 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace controlApp1 
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+class Program
+{
+    static void Main(string[] args) {
+        Console.ReadKey();
+        string Name = "Iman";
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        Console.WriteLine(c);
 
-            Console.WriteLine($"Hello, {name}!");
-
-            int age = GetAge();
-            Console.WriteLine($"You are {age} years old.");
-
-            bool isAdult = CheckIfAdult(age);
-            Console.WriteLine($"Are you an adult? - {isAdult}");
-
-            string favoriteColor = GetFavoriteColor();
-            Console.WriteLine($"Your favorite color is {favoriteColor}");
-
-            int currentYear = DateTime.Now.Year;
-            int birthYear = CalculateBirthYear(age, currentYear);
-            Console.WriteLine($"You were born in {birthYear}");
-        }
-
-        static int GetAge()
-        {
-            Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            return age;
-        }
-
-        static bool CheckIfAdult(int age)
-        {
-            return age >= 18;
-        }
-
-        static string GetFavoriteColor()
-        {
-            Console.Write("Enter your favorite color: ");
-            string favoriteColor = Console.ReadLine();
-            return favoriteColor;
-        }
-
-        static int CalculateBirthYear(int age, int currentYear)
-        {
-            return currentYear - age;
-        }
+        
     }
+} 
+class Iman 
+{
+}
 }

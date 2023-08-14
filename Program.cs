51ccd57e0dc9019838a253ec06df7b22f28e5a67
alireza babaie <60476541+babaie774@users.noreply.github.com>
2023-08-14 21:25:26 +0330

    working-with-byte

--- Program.cs
+++ Program.cs
@@ -1,45 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using Microsoft.Data.SqlClient;
 
 namespace controlApp1 
 {
-class Program
-{
-    static void Main(string[] args) {
-        // Console.ReadKey();
-        // string Name = "Iman";
-        // int a = 1;
-        // int b = 2;
-        // int c = a + b;
-        // Console.WriteLine(c);
-
-        int a;
-        int b;
-    try {
-        Console.WriteLine("inter Number:");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("inter Number:");
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(a+b);
-    } catch(FormatException) {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("formatException");
-        Console.ResetColor();
-    } catch(SqlException) {
-        Console.WriteLine("sqlException");
-    } catch {
-         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("please just write number");
-        Console.ResetColor();
-    } finally {
-        Console.WriteLine("finished!");
-    }
-    }
-} 
-    class Iman 
-    {
-    }
+    class Program {
+        static void Main(string[] args) {
+            checked
+            {
+                // byte number = 255;
+                // number = number + 1;
+                int number = 0;
+                Console.WriteLine(number + " " + byte.MinValue + " " + byte.MaxValue);
+            }
+        }
+    } 
 }

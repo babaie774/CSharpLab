b3d843ee8b4201391de8bf8d9208b3928cb4d576
alireza babaei <babaie774@gmail.com>
2024-09-16 09:34:02 +0330

    feat: create-simple-class

--- MyClass.cs
+++ MyClass.cs
@@ -5,25 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Program {
-    //private - default
-    //public
-    //protected - just acess in the namespace
-
-   class FullName {
-        public static string FullNameGenerator() {
-                string FullName = MyClass.FullName("alireza","ahmad");
+        public class Person {
+            public Person(string name,string lastName)
+            {
+                Name = name;
+                LastName = lastName;
+            }
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string FullName { 
+                get{
+                return Name + LastName;
+                }
+            }
+            public override string ToString()
+            {
                 return FullName;
-        }
-   }
-
-
-    class MyClass {
-        public static int Sum(int a ,int b) {
-            return a + b;
+            }
         }
 
-        public static string FullName(string a, string b) {
-            return a + b;
-        }
-    } 
 }
--- Program.cs
+++ Program.cs
@@ -12,18 +12,8 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            // HelloUser();
-            // HelloUser("alireza");
-            // Console.WriteLine(Sum(1,2));
-            // string fullName = FullName.FullNameGenerator();
-            // Console.WriteLine(fullName);
-            // Car pride = new Car();
-            // pride.CarName = "pride";
-            // pride.CarSpeed = 120;
-            // pride.CarModel = "sipa";
-            // Console.WriteLine(pride.CarName);
-            Person alireza = new Person(0, "alireza", "babaei");
-            Console.WriteLine(alireza.name);
+            Person person = new Person("Alireza","Babaei");
+            Console.WriteLine(person);
         }
 
         static void HelloUser()
